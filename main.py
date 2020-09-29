90ff0a9e4a3728554de22298acc94c9fd5f85467
Igor Lousada <igorcoutinho97@gmail.com>
2020-09-29 17:13:25 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from teste import criar_conta,deposita,saca,extrato
+
+conta = criar_conta(540,'Igor Lousada',50000,3000)
+
+
+novo_deposito = deposita(conta,2    000)
+novo_saque = saca(conta,1000)
+
+novo_extrato = extrato(conta)
+
